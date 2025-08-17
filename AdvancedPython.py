1e66bac6b884ac0eba59ce424ccfbf048ebcc7d2
Goutham Veernapalli <gouthamrv@gmail.com>
2025-08-17 18:31:03 +0530

    Unpacking variables test

---
+++ AdvancedPython.py
@@ -0,0 +1,17 @@
+
+#Unpacking with * (splat operator)
+nums = [1, 2, 3, 4, 5]
+first, *middle, last = nums
+print(first, middle, last)  # 1 [2, 3, 4] 5
+
+# Unpacking with * in function arguments
+def func(*args):
+    print(args)
+
+func(1, 2, 3)  # (1, 2, 3)
+
+# Unpacking with ** (double splat operator)
+def func(**kwargs):
+    print(kwargs)
+
+func(a=1, b=2, c=3)  # {'a': 1, 'b': 2, 'c': 3}     
